ab1fa827ed68884b90466a3ec5316feda9e01f0d
Kai Chang <kaijchang@gmail.com>
2020-07-14 13:15:08 -0700

    use single quotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './App';
 
-import "./index.css";
+import './index.css';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
@@ -18,7 +18,7 @@ ReactDOM.render(
             </Switch>
         </Router>
     </React.StrictMode>,
-    document.getElementById("root")
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
