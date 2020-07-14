931675755f074c98c2e86cfe99ecf06e1cac7c3e
Kai Chang <kaijchang@gmail.com>
2020-07-14 15:57:29 -0700

    setup colors and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import IndexPage from './pages/index';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@chakra-ui/core';
 
+import IndexPage from './pages/index';
+
+import theme from './theme';
+
 function App() {
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={ theme }>
             <Router>
                 <Switch>
                     <Route path="/">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { Box, Heading, Text } from '@chakra-ui/core';
+
 const IndexPage = () => (
-    <div>
-        Hello World!
-    </div>
+    <Box>
+        <Heading color='petcode.blue'>Heading</Heading>
+        <Text color='petcode.teal'>Body</Text>
+    </Box>
 );
 
 export default IndexPage;
