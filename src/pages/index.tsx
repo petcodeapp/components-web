75751cf7ebc506d1ba3cf4588c10a36c18f01247
Kai Chang <kaijchang@gmail.com>
2020-07-14 19:35:48 -0700

    move fonts to their own folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,34 +4,34 @@ import { Box, Button, Flex, Heading, Icon, Image, Input, InputGroup, InputLeftEl
 
 const IndexPage = () => (
     <Flex direction='row' height='100%'>
-        <Flex direction='column' flexGrow={1} flexBasis='calc(50% - 96px)' alignItems='center' justifyContent='center' marginX='32'>
+        <Flex direction='column' flexGrow={1} flexBasis='calc(50% - 96px)' alignItems='center' justifyContent='center' marginX={32}>
             <Text color='petcode.neutral.400' fontSize='2xl' fontWeight='bold' textTransform='uppercase' margin={0}>
                 Welcome to
             </Text>
             <Box>
-                <Heading as='span' color='petcode.yellow' fontSize='logo' margin='0'>
+                <Heading as='span' color='petcode.yellow' fontSize='logo' margin={0}>
                     Pet
                 </Heading>
-                <Heading as='span' color='petcode.blue.400' fontSize='logo' margin='0'>
+                <Heading as='span' color='petcode.blue.400' fontSize='logo' margin={0}>
                     Code
                 </Heading>
             </Box>
             <Text color='petcode.neutral.600' fontSize='3xl' fontWeight='thin' textAlign='center' lineHeight='shorter' margin={0}>
                 Log in to view and update your pet profile with the latest information.
             </Text>
-            <Flex direction='column' alignItems='center' width='65%' marginY='3'>
-                <InputGroup size='lg' width='100%' marginY='2'>
+            <Flex direction='column' alignItems='center' width='65%' marginY={3}>
+                <InputGroup size='lg' width='100%' marginY={2}>
                     <InputLeftElement color='petcode.neutral.400' children={ <Icon size='25px' name='username' color='petcode.neutral.400'/> }/>
                     <Input rounded='full' placeholder='Username' color='petcode.neutral.700' borderColor='petcode.neutral.400' focusBorderColor='petcode.neutral.500'/>
                 </InputGroup>
-                <InputGroup size='lg' width='100%' marginY='2'>
+                <InputGroup size='lg' width='100%' marginY={2}>
                     <InputLeftElement children={ <Icon size='25px' name='password' color='petcode.neutral.400'/> }/>
                     <Input rounded='full' placeholder='Password' color='petcode.neutral.700' borderColor='petcode.neutral.400' focusBorderColor='petcode.neutral.500'/>
                 </InputGroup>
-                <Text color='petcode.blue.400' fontSize='lg' alignSelf='flex-end' margin='0'>
+                <Text color='petcode.blue.400' fontSize='lg' alignSelf='flex-end' margin={0}>
                     <Link>Forgot Password?</Link>
                 </Text>
-                <Button size='lg' variantColor='petcode.blue' variant='solid' border='none' rounded='full' width='100%' marginTop='3' height='57px'> 
+                <Button size='lg' variantColor='petcode.blue' variant='solid' border='none' rounded='full' width='100%' marginTop={3} height='57px'> 
                     <Text fontSize='xl' fontWeight='thin' textTransform='uppercase'>Sign In</Text>
                 </Button>
                 <Text color='petcode.neutral.500' fontSize='lg'>
@@ -48,7 +48,7 @@ const IndexPage = () => (
         >
             <Box flexGrow={1}/>
             <Image alt='PetCode Logo' src='/media/petcode-logo.png'/>
-            <Heading color='petcode.neutral.700' fontSize='4xl' fontWeight='thin' textAlign='center' marginX='24'>
+            <Heading color='petcode.neutral.700' fontSize='4xl' fontWeight='thin' textAlign='center' marginX={24}>
                 One Code. An endless suite of features for pet owners.
             </Heading>
             <Box flexGrow={3}/>
