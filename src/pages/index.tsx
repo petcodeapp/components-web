8607d656385d30446699a3f89a9d8b4e16acdd3f
Kai Chang <kaijchang@gmail.com>
2020-07-14 17:03:41 -0700

    finish top text section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
-import { Box, Heading, Text } from '@chakra-ui/core';
+import { Box, Flex, Heading, Input, Text } from '@chakra-ui/core';
 
 const IndexPage = () => (
-    <Box>
-        <Heading color='petcode.blue'>Heading</Heading>
-        <Text color='petcode.teal'>Body</Text>
-    </Box>
+    <Flex direction='row' height='100%'>
+        <Flex direction='column' flexBasis='50%' alignItems='center' justifyContent='center' paddingX='32'>
+            <Text color='petcode.neutral.400' fontSize='2xl' fontWeight='bold' textTransform='uppercase' margin={0}>
+                Welcome to
+            </Text>
+            <Box>
+                <Heading as='span' color='petcode.yellow' fontSize='logo' margin={0}>
+                    Pet
+                </Heading>
+                <Heading as='span' color='petcode.blue' fontSize='logo' margin={0}>
+                    Code
+                </Heading>
+            </Box>
+            <Text color='petcode.neutral.600' fontSize='3xl' fontWeight='thin' textAlign='center' lineHeight='shorter' margin={0}>
+                Log in to view and update your pet profile with the latest information.
+            </Text>
+        </Flex>
+        <Flex direction='column' flexBasis='50%'>
+
+        </Flex>
+    </Flex>
 );
 
 export default IndexPage;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -7,12 +7,29 @@ export default {
         heading: '"Lilita One", sans-serif',
         body: "Nunito, sans-serif"
     },
+    fontSizes: {
+        ...theme.fontSizes,
+        logo: '125px'
+    },
+    fontWeights: {
+        ...theme.fontWeights,
+        thin: 300
+    },
     colors: {
         ...theme.colors,
         petcode: {
-          blue: '#51BCDA',
-          yellow: '#FBC658',
-          teal: '#51CBCE'
+            blue: '#51BCDA',
+            yellow: '#FBC658',
+            teal: '#51CBCE',
+            neutral: {
+                100: '#F7FAFC',
+                200: '#EDF2F7',
+                300: '#E2E8F0',
+                400: '#CBD5E0',
+                500: '#A0AEC0',
+                600: '#718096',
+                700: '#4A5568'
+            },
         }
     }
 };
